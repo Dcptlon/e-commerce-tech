482f378fa69443df2935999e9cf1a1e44278bda9
Elias <eliasarzamendia015@gmail.com>
2026-01-26 00:41:52 -0300

    feat: setup routing structure with React Router
    - Install react-router
    - Configure BrowserRouter in main.jsx
    - Create route definitions in App.jsx
    - Add placeholder pages for all main routes (MainLayout, Home, Products, ProductDetail, BestSellers, Cart, Login)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
+import { Routes, Route } from 'react-router'
+import MainLayout from './layout/MainLayout'
+import Home from './pages/Home'
 
 function App() {
   
   return (
-    <>
-      
-    </>
+      <Routes>
+        <Route path='/' element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/product/:id" element={<ProductDetailPage />} />
+          <Route path="/best-sellers" element={<BestSellersPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Route>
+      </Routes>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>Hola soy el home</div>
+  )
+}
+
+export default Home
