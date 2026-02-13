8810bc78067fc6750d7f19a1d42573688897384f
Elias <eliasarzamendia015@gmail.com>
2026-02-13 17:16:32 -0300

    feat: add ImageCarousel component with styles

---
+++ src/components/image-carousel/ImageCarousel.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+import styles from './imageCarousel.module.css'
+
+function ImageCarousel({ images = [] }) {
+  const [current, setCurrent] = useState(0)
+
+  if (!images.length) return null
+
+  return (
+    <div className={styles.carouselContainer}>
+      <img src={images[current]} alt="Imagen del producto" />
+
+      <div>
+        {images.map((_, index) => (
+          <button
+            key={index}
+            onClick={() => setCurrent(index)}
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default ImageCarousel
