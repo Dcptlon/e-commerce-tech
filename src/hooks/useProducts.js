f3499da17fe8339c576d09442a3a32db77ba262a
Elias <eliasarzamendia015@gmail.com>
2026-02-16 00:58:06 -0300

    feat: enhance useProducts hook to accept options and add useProductBySlug hook

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,9 +1,19 @@
 import { useQuery } from '@tanstack/react-query'
 import { getProducts } from '../services/productService.js'
+import { getProductBySlug } from '../services/productService.js'
 
-export function useProducts(filters = {}) {
+export function useProducts(filters = {}, options = {}) {
   return useQuery({
     queryKey: ['products', filters],
     queryFn: () => getProducts(filters),
+    ...options
+  })
+}
+
+export function useProductBySlug(slug) {
+  return useQuery({
+    queryKey: ['product', slug],
+    queryFn: () => getProductBySlug(slug),
+    enabled: !!slug,
   })
 }
